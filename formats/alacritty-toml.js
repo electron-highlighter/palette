c8d494798cf9322e64ce643339c19854ddb05cf8
Mike McBride <mike@mcbrides.us>
2026-06-04 09:29:52 -0500

    refactor: read ANSI slots uniformly from ansi[] in Alacritty emitter

--- formats/alacritty-toml.js
+++ formats/alacritty-toml.js
@@ -1,16 +1,19 @@
+// Alacritty TOML. ANSI slots read from palette.ansi[0..15] (the canonical slot
+// map); ansi[0]/[7]/[8]/[15] = lightGray/dim/comment/foreground per the
+// light-terminal convention. base.* drives the non-ANSI primary/cursor/selection.
 module.exports = function render(p) {
-  const { ansi, base, accents } = p
+  const { ansi, base } = p
   return `# Colors (Electron Highlighter Day)
 
 [colors.bright]
 black = '${ansi[8]}'
-blue = '${accents.blue}'
-cyan = '${accents.cyan}'
-green = '${accents.green}'
-magenta = '${accents.purple}'
-red = '${accents.red}'
+blue = '${ansi[12]}'
+cyan = '${ansi[14]}'
+green = '${ansi[10]}'
+magenta = '${ansi[13]}'
+red = '${ansi[9]}'
 white = '${ansi[15]}'
-yellow = '${accents.yellow}'
+yellow = '${ansi[11]}'
 
 [colors.cursor]
 cursor = '${base.cursor}'
@@ -18,13 +21,13 @@ text = '${base.cursorText}'
 
 [colors.normal]
 black = '${ansi[0]}'
-blue = '${accents.blue}'
-cyan = '${accents.cyan}'
-green = '${accents.green}'
-magenta = '${accents.purple}'
-red = '${accents.red}'
+blue = '${ansi[4]}'
+cyan = '${ansi[6]}'
+green = '${ansi[2]}'
+magenta = '${ansi[5]}'
+red = '${ansi[1]}'
 white = '${ansi[7]}'
-yellow = '${accents.yellow}'
+yellow = '${ansi[3]}'
 
 [colors.primary]
 background = '${base.background}'
