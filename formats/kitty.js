7b83448daa628e24d4120f3c25fa389dd8efec37
Mike McBride <mike@mcbrides.us>
2026-06-04 09:36:35 -0500

    feat: add Kitty emitter

---
+++ formats/kitty.js
@@ -0,0 +1,34 @@
+module.exports = function render(p) {
+  const { ansi, base, accents, roles } = p
+  const head = `# vim:ft=kitty
+
+## name: Electron Highlighter Day
+## license: MIT
+## author: Mike McBride
+## upstream: https://github.com/electron-highlighter/kitty
+
+background ${base.background}
+foreground ${base.foreground}
+selection_background ${base.selection}
+selection_foreground ${base.foreground}
+url_color ${roles.hyperLink}
+cursor ${base.cursor}
+cursor_text_color ${base.cursorText}
+
+# Tabs
+active_tab_background ${accents.blue}
+active_tab_foreground ${base.background}
+inactive_tab_background ${roles.hyperTabNavBg}
+inactive_tab_foreground ${base.dim}
+
+# Windows
+active_border_color ${accents.blue}
+inactive_border_color ${base.lightGray}
+`
+  const normal = ['\n# normal']
+  for (let i = 0; i <= 7; i++) normal.push(`color${i} ${ansi[i]}`)
+  const bright = ['\n# bright']
+  for (let i = 8; i <= 15; i++) bright.push(`color${i} ${ansi[i]}`)
+  const extended = ['\n# extended colors', `color16 ${ansi[16]}`, `color17 ${ansi[17]}`]
+  return head + normal.join('\n') + '\n' + bright.join('\n') + '\n' + extended.join('\n') + '\n'
+}
--- targets.js
+++ targets.js
@@ -4,6 +4,7 @@ const targets = [
   { name: 'alacritty-toml', dest: '../alacritty/electron-highlighter-day.toml', render: require('./formats/alacritty-toml') },
   { name: 'alacritty-yaml', dest: '../alacritty/electron_highlighter_day.yml', render: require('./formats/alacritty-yaml') },
   { name: 'ghostty', dest: '../ghostty/electron-highlighter-day', render: require('./formats/ghostty') },
+  { name: 'kitty', dest: '../kitty/electron_highlighter_day.conf', render: require('./formats/kitty') },
 ]
 
 module.exports = { palette, targets }
